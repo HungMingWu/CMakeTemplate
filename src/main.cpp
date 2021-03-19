acaf39cbb4b8563b76fe03a93a2252d4440ddf30
HungMingWu <u9089000@gmail.com>
2021-03-19 16:31:55 +0800

    Append VCPKG manifest support

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
-﻿// CMakeTemplate.cpp: 定義應用程式的進入點。
-//
-
-#include <iostream>
-
-using namespace std;
+﻿#include <iostream>
+#include "fmt/format.h"
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	std::cout << fmt::format("Hello {}.\n", "CMake");
 	return 0;
 }
